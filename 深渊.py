ed3e279f785dd09418bd7a6fb60fac023335fa35
demonknight <762853138@qq.com>
2026-03-12 16:21:32 +0800

    version11

--- 深渊.py
+++ 深渊.py
@@ -32,11 +32,11 @@ def rand_sleep_2h_range():
 
 # ------------------ 启动延迟 ------------------
 print("程序启动，5秒后开始执行...")
-rand_sleep(5, 0.1)  # 启动延迟 ±10%
+rand_sleep(2, 0.1)  # 启动延迟 ±10%
 
 # ------------------ 初始化操作 1-5 ------------------
 rand_double_click(180, 1144)       # 1 双击
-rand_sleep(10, 0.2)                # 2 等待约10秒 ±20%
+rand_sleep(15, 0.2)                # 2 等待约10秒 ±20%
 rand_click(1497, 1019)             # 3 点击
 rand_sleep(2, 0.2)
 rand_click(1350, 451)              # 4 点击
@@ -57,10 +57,10 @@ while True:
     print(f"点击完成，时间：{now}")
 
     # 10 最小化
-    rand_click(1746, 1419)
+    rand_click(1789, 1419)
 
     print("等待约2小时~2小时5分钟...")
     rand_sleep_2h_range()           # 主循环等待
 
     # 恢复窗口
-    rand_click(1746, 1419)
+    rand_click(1789, 1419)
