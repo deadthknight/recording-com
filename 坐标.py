52e52bf41de05ab0102a0c0a7ee705a234af22ad
demonknight <762853138@qq.com>
2026-03-12 11:05:52 +0800

    version1

---
+++ 坐标.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python3.11    # Python解释器
+# -*- coding: utf-8 -*-
+import pyautogui
+import time
+
+while True:
+    print(pyautogui.position())
+    time.sleep(1)
+
+if __name__ == "__main__":
+    pass
